8fb895d0e474e8ae5056be7f1f1076778efbf07b
jeevandas-jd <jeevandasms2004@gmail.com>
2026-09-07 13:17:26 +0530

    v5

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "placeprep.settings")
+
+from placeprep.wsgi import application
